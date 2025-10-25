df7019ccde05221cc48ffecaabd05b0a69f803ae
Nithish <nithishkumar.g@pmdgtech.com>
2025-10-25 18:43:01 +0530

    crud operation done using in memory list.

--- main.py
+++ main.py
@@ -12,10 +12,34 @@ products =[
     Product(id=2,name="Laptop",description="",price=200000,quantity=5)
 ]
 
-@app.get("/api/products",tags=["User"])
+@app.get("/api/products",tags=["Product"])
 def getProducts():
     return products
 
-@app.post("/api/products",tags=["User"])
-def createProduct():
-    return products
+@app.post("/api/product",tags=["Product"])
+def createProduct(product: Product):
+    products.append(product)
+    return product
+
+@app.get("/api/product/{id}",tags=["Product"])
+def getProduct(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+    return {"message": "Product not found"}
+
+@app.put("/api/product/{id}",tags=["Product"])
+def updateProduct(id: int, updatedProduct: Product):
+    for index, product in enumerate(products):
+        if product.id == id:
+            products[index] = updatedProduct
+            return updatedProduct
+    return {"message": "Product not found"}
+
+@app.delete("/api/product/{id}",tags=["Product"])
+def deleteProduct(id: int):
+    for index, product in enumerate(products):
+        if product.id == id:
+            deleted_product = products.pop(index)
+            return deleted_product
+    return {"message": "Product not found"}
