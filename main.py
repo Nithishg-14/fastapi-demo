33704bd40fe62f9324ce608c5aeebe44c3d5f137
Nithish <nithishkumar.g@pmdgtech.com>
2025-10-26 18:33:09 +0530

    DB CRUD api implementation.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from models import Product
 from database import session,engine
 import database_models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 database_models.Base.metadata.create_all(bind=engine)
@@ -14,6 +15,24 @@ products =[
     Product(id=1,name="Phone",description="",price=20000,quantity=2),
     Product(id=2,name="Laptop",description="",price=200000,quantity=5)
 ]
+
+def init_db():
+    count = session().query(database_models.Product).count()
+    if count == 0:
+        db = session()
+        for product in products:
+            db_product = database_models.Product(**product.model_dump())
+            db.add(db_product)
+        db.commit()
+
+init_db()
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/api/products",tags=["Product"])
 def getProducts():
@@ -48,6 +67,41 @@ def deleteProduct(id: int):
     return {"message": "Product not found"}
 
 @app.get("/api/v2/products",tags=["Product"])
-def getProducts():
-    db = session()
-    return products
+def getProducts(db: Session = Depends(get_db)):
+    db_products = db.query(database_models.Product).all()
+    return db_products
+
+@app.get("/api/v2/product/{id}",tags=["Product"])
+def getProduct(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
+    return {"message": "Product not found"}
+
+@app.post("/api/v2/product",tags=["Product"])
+def createProduct(product:Product,db:Session = Depends(get_db)):
+    db_product = database_models.Product(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    return product
+
+@app.put("/api/v2/product/{id}",tags=["Product"])
+def updateProduct(id:int, product:Product, db:Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        db_product.name = product.name
+        db_product.description = product.description
+        db_product.price = product.price
+        db_product.quantity = product.quantity
+        db.commit()
+        return product
+    return {"message":"Product not found"}
+
+@app.delete("/api/v2/product/{id}",tags=["Product"])
+def deleteProduct(id:int, db:Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+        return {"message":"Product deleted successfully"}
+    return {"message":"Product not found"}
